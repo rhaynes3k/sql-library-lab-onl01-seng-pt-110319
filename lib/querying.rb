f2ce391ee70f0eb1bc028a4495561dd959e37101
Rommel Haynes <rhaynes3k@gmail.com>
2020-01-30 22:05:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,15 +30,15 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT char.name, COUNT(bks.title) AS bkCnt
-  FROM Characters AS char 
-  LEFT OUTER JOIN Series AS ser ON ser.author_id = char.author_id 
-  JOIN Books AS bks ON bks.series_id = ser.id GROUP BY char.name 
+  "SELECT Characters.name, COUNT(bks.title) AS bkCnt
+  FROM Characters
+  LEFT OUTER JOIN Series AS ser ON ser.author_id = Characters.author_id 
+  JOIN Books AS bks ON bks.series_id = ser.id GROUP BY Characters.name 
   HAVING COUNT(bks.title) = (
   SELECT MAX(bkCnt)
   FROM (
-  SELECT char.name, COUNT(bks.title) bkCnt
+  SELECT Characters.name, COUNT(bks.title) bkCnt
   FROM Characters
-  GROUP BY char.name)
+  GROUP BY Characters.name)
   )";
 end
